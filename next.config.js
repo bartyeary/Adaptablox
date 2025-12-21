434ad79f066142e15c3649371269dce4282661ac
bartyeary <bart.yeary@gmail.com>
2025-12-21 14:07:32 -0800

    fixed index pointer

--- next.config.js
+++ next.config.js
@@ -1,16 +1,9 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '3845',
-        pathname: '/assets/**',
-      },
-    ],
+    unoptimized: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
 
