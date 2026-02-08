5cb35e29048fae0b44cbf3b956096723db3dd701
bartyeary <bart.yeary@gmail.com>
2026-02-07 20:13:43 -0800

    fixed CSS finally

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '',
-  assetPrefix: '/',
-  trailingSlash: true,
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
