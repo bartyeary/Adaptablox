0bad83236c194eb6fc18f9ac0d7e29e08edc8750
bartyeary <bart.yeary@gmail.com>
2026-02-07 18:10:36 -0800

    clean build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+
+  // GitHub Pages serves from /docs
+  basePath: '/docs',
+  assetPrefix: '/docs',
+
   images: {
     unoptimized: true,
   },
