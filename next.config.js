81b15713ade8863b62fd8eef0638dab324deffe7
bartyeary <bart.yeary@gmail.com>
2026-02-07 19:34:32 -0800

    fixed CSS pathing

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+
+  // REQUIRED for GitHub Pages project sites
+  assetPrefix: '',
+
   images: {
     unoptimized: true,
   },
