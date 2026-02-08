33d7cc3b9bc0bf2156a06c5d34d290f8c6615fa6
bartyeary <bart.yeary@gmail.com>
2026-02-07 19:40:57 -0800

    css and 404 resolved?

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-
-  // REQUIRED for GitHub Pages project sites
-  assetPrefix: '',
-
-  images: {
-    unoptimized: true,
-  },
+  basePath: '',
+  assetPrefix: '/',
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
