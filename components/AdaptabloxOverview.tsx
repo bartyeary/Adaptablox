485ee0dbe1f29ab9cd12c5ba34eec9e157c122b3
bartyeary <bart.yeary@gmail.com>
2026-07-05 19:21:24 -0700

    more responsive ascii

--- components/AdaptabloxOverview.tsx
+++ components/AdaptabloxOverview.tsx
@@ -113,7 +113,7 @@ export default function AdaptabloxOverview() {
           </p>
           <div className="[container-type:inline-size] -mx-[17px] w-[calc(100%+34px)] md:-mx-[24px] md:w-[calc(100%+48px)] overflow-x-hidden md:overflow-x-auto">
             <pre
-              className="font-mono not-italic relative text-[#4e4e4e] w-full whitespace-pre text-center leading-[1.15] overflow-x-hidden [font-size:calc(100cqw/72)] md:text-[15px] md:leading-[18px]"
+              className="font-mono not-italic relative text-[#4e4e4e] w-full whitespace-pre text-center leading-[1.15] overflow-x-hidden [font-size:calc(100cqw/69.75)] md:text-[15px] md:leading-[18px]"
               data-node-id="42:802"
               style={{ fontFamily: 'monospace' }}
             >
